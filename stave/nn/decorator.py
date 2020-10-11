f893bab5f106c252d68f63fbe0821216b4bb817a
SunDoge <384813529@qq.com>
2020-10-11 11:16:35 +0800

    refactor(decorator.py): 重写了differentiable的逻辑，现在可以缓存keys

--- stave/nn/decorator.py
+++ stave/nn/decorator.py
@@ -1,56 +1,88 @@
-from dataclasses import dataclass
-from typing import List, Dict
+from dataclasses import Field, dataclass, field
+from typing import Any, List, Dict, Type, TypeVar, Tuple
 from .modules import Module
 from jax.tree_util import register_pytree_node
+import logging
 
 
-def differentiable(cls: Module, use_dataclass=True):
-    # @functools.wraps(cls)
-    # def wrapper(*args, **kwargs):
-    #     """
-    #     Rewrite __init__ of class
-    #     store args and kwargs
-    #     """
-    #     obj = cls(*args, **kwargs)
-    #     obj._args = args
-    #     obj._kwargs = kwargs
-    #     return obj
-    if use_dataclass:
-        cls = dataclass(cls, repr=False)
+_logger = logging.getLogger(__name__)
 
-    def flatten(obj: Module) -> (List[Dict], Dict):
-        """
-        get args, kwargs of obj as aux_data
-        get params of obj as children
-        return aux_data, children
-        """
 
-        children, aux_data = obj.deconstruct()
+_FIELDS = '__dataclass_fields__'
+_NODE_TYPE = 'node_type'
+_CHILDREN = 'children'
+_AUXILIARY_DATA = 'auxiliary_data'
 
-        # print('=' * 100)
-        # print(obj, children)
-        # print('=' * 100)
-        return [children], aux_data
 
-    def unflatten(aux_data: Dict, children: List[Dict]):
-        """
-        obj = cls(aux_data)
-        obj.load(children)
-        return obj
-        """
+class NodeType:
+    PARAMETER = 'Parameter'
+    BUFFER = 'Buffer'
+    CONSTANT = 'Constant'
 
-        aux_data.update(children[0])
-        # print(cls, aux_data)
-        obj = cls(
-            **aux_data
-        )
 
-        return obj
+PARAMETER = {
+    _NODE_TYPE: NodeType.PARAMETER
+}
 
-    register_pytree_node(
-        cls,
-        flatten,
-        unflatten,
-    )
+BUFFER = {
+    _NODE_TYPE: NodeType.BUFFER
+}
+
+CONSTANT = {
+    _NODE_TYPE: NodeType.CONSTANT
+}
+
+T = TypeVar('T')
+Keys = Dict[str, List[str]]
+
+
+def _get_keys(node: Any) -> Keys:
+    """
+    记录哪些变量是children，哪些是aux data
+    """
+    keys: Keys = {
+        _CHILDREN: [],
+        _AUXILIARY_DATA: [],
+    }
+
+    fields: Dict[str, Field] = getattr(node, _FIELDS)
+
+    for key, value in fields.items():
+        node_type = value.metadata.get(_NODE_TYPE, NodeType.CONSTANT)
+        if node_type == NodeType.PARAMETER or node_type == NodeType.BUFFER:
+            keys[_CHILDREN].append(key)
+        else:
+            keys[_AUXILIARY_DATA].append(key)
+
+    return keys
+
+
+def differentiable(cls: Type[T]) -> Type[T]:
+
+    keys = _get_keys(cls)
+
+    def _tree_flatten(node: Module) -> Tuple[Tuple[Dict[str, Any]], Dict[str, Any]]:
+        children = {}
+        aux_data = {}
+        for key in keys[_CHILDREN]:
+            children[key] = getattr(node, key)
+
+        for key in keys[_AUXILIARY_DATA]:
+            aux_data[key] = getattr(node, key)
+
+        _logger.debug('=' * 50)
+        _logger.debug('flatten: %s', cls)
+        _logger.debug('aux_data: %s', aux_data)
+        _logger.debug('children: %s', children)
+        return (children,), aux_data
+
+    def _tree_unflatten(aux_data: Tuple[Dict[str, Any]], children: Dict[str, Any]) -> Module:
+        _logger.debug('=' * 50)
+        _logger.debug('unflatten: %s', cls)
+        _logger.debug('aux_data: %s', aux_data)
+        _logger.debug('children: %s', children)
+        return cls(**aux_data, **children[0])  # type: ignore
+
+    register_pytree_node(cls, _tree_flatten, _tree_unflatten)
 
     return cls
